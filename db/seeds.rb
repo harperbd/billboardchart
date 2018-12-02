8417f7531509bc89ab5d55b308e360a9efef3fe6
Brad.Harper <Brad.Harper@consensuscorp.com>
2018-12-01 18:48:10 -0700

    resolved through association for billboard and song rankings

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@ billboard = Billboard.create(name: 'Top 100')
 5.times do |i|
   artist = Artist.create(name: "artist_#{i}", genre: "genre_#{i}")
 
-  2.times do |i|
-    song = Song.create(title: "title_#{i}", billboard_id: billboard.id, artist_id: artist.id)
+  2.times do |j|
+    song = Song.create(title: "title_#{j}", billboard_id: billboard.id, artist_id: artist.id)
   end
 
 end
