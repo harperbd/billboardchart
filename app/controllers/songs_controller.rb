795264a472621a5723663efec7892b372d1dc417
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2018-12-02 16:08:45 -0700

    CRUD actions for billboard, song, artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,70 @@
 class SongsController < ApplicationController
+  before_action :artist_or_billboard
+  before_action :set_song, only: [:show, :update]
   def index
-    @songs = @billboard.songs
+    @songs = @art_or_bill.songs
   end
 
   def show
   end
 
   def new
+    # @song = @billboard.songs.new
+    @song = @art_or_bill.songs.new
+    render partial: "form"
   end
+
+  def create
+    @song = @art_or_bill.songs.create(song_params)
+    if @song.save
+      if @song.billboard_id == nil
+         @song.update(billboard_id: 1) 
+      end
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  # def rank
+  #   @curr_rank = @billboard.songs.rank
+  # end
+
+  private
+    def song_params
+      params.require(:song).permit(:title, :artist_id, :billboard_id, :rank)
+    end
+
+    # def set_billboard
+    #   @billboard = Billboard.find(params[:billboard_id])
+    # end
+
+    # def set_artist
+    #   @artist = Artist.find(params[:artist_id])
+    # end
+
+    # def set_song
+    #   @song = Song.find(params[:id])
+    # end
+
+    def artist_or_billboard
+      @art_or_bill ||=
+        if params[:artist_id]
+          Artist.find(params[:artist_id])
+        elsif params[:billboard_id]
+          Billboard.find(params[:billboard_id])
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-billboard = Billboard.create(name: 'Top 100')
+# billboard = Billboard.create(name: 'Top 100')
 
-5.times do |i|
-  artist = Artist.create(name: "artist_#{i}", genre: "genre_#{i}")
+# 5.times do |i|
+#   artist = Artist.create(name: "artist_#{i}", genre: "genre_#{i}")
 
-  2.times do |i|
-    song = Song.create(title: "title_#{i}", billboard_id: billboard.id, artist_id: artist.id)
-  end
+#   2.times do |i|
+#     song = Song.create(title: "title_#{i}", billboard_id: billboard.id, artist_id: artist.id)
+#   end
 
-end
+# end
+
+
+# billboard = Billboard.new
+# artist = Artist.create(name: "Alt-J", genre: "alternative")
+# song = Song.create(title: "Interlude 1", billboard_id: billboard.id, artist_id: artist.id)
