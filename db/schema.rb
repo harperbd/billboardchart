394eae6e9bcd7000b6d37133fd51b44ea819bdbb
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2018-12-03 21:02:50 -0700

    dropped DB, got controller running

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_233429) do
+ActiveRecord::Schema.define(version: 2018_11_30_034959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,13 @@ ActiveRecord::Schema.define(version: 2018_12_03_233429) do
   end
 
   create_table "billboard_song_rankings", force: :cascade do |t|
+    t.integer "ranking"
+    t.bigint "song_id"
+    t.bigint "billboard_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["billboard_id"], name: "index_billboard_song_rankings_on_billboard_id"
+    t.index ["song_id"], name: "index_billboard_song_rankings_on_song_id"
   end
 
   create_table "billboards", force: :cascade do |t|
@@ -36,12 +41,11 @@ ActiveRecord::Schema.define(version: 2018_12_03_233429) do
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.bigint "artist_id"
-    t.bigint "billboard_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "rank"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
-    t.index ["billboard_id"], name: "index_songs_on_billboard_id"
   end
 
+  add_foreign_key "billboard_song_rankings", "billboards"
+  add_foreign_key "billboard_song_rankings", "songs"
 end
