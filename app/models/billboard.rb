4760cfb363d92eef7509edfa5503a53a83e57a64
Brad.Harper <Brad.Harper@consensuscorp.com>
2018-12-05 10:43:11 -0700

    making sure to delete children (belongs_to)

--- app/models/billboard.rb
+++ app/models/billboard.rb
@@ -1,4 +1,4 @@
 class Billboard < ApplicationRecord
-  has_many :billboard_song_rankings
+  has_many :billboard_song_rankings, dependent: :destroy
   has_many :songs, through: :billboard_song_rankings
 end
